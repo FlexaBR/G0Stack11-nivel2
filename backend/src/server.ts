17e45ed5f889e51f63a6469d77f8b85ad5999074
FlexaBR <r.gutheil@terra.com.br>
2020-04-16 11:38:18 -0300

    fim nível 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,38 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+// Middleware de tratamento de erros tem que vir depois das rotas, pois os
+// erros acontecem nas rotas.
+// Middlewares de erros recebem 4 parâmetros.
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on  port 3333!  ');
